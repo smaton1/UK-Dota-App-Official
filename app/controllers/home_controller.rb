99be659ab7b147f663caddd8fbe1055a9d6a4d5c
Smaton1 <sam_maton@hotmail.com>
2014-09-10 13:34:41 +0100

    changed the styling on the profile page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+
     @matchlist = []
     if session.key? :current_user
       url = URI.parse("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=#{ENV['STEAM_WEB_API_KEY']}&account_id=#{session[:current_user][:uid]}")
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
+    @matchlist = []
+    if session.key? :current_user
+      url = URI.parse("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=#{ENV['STEAM_WEB_API_KEY']}&account_id=#{session[:current_user][:uid]}")
+      res = Net::HTTP::get(url)
+      @matchlist = JSON.load(res)['result']['matches'] || []
+    end
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
