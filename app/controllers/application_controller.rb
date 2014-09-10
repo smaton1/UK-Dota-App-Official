9cdbdf6a7346d30b4f17ebe0c7104438dc682614
Smaton1 <sam_maton@hotmail.com>
2014-09-10 11:31:51 +0100

    added the user model and allow for steam oauth to save data to the database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :current_user
+  def current_user
+
+    @current_user ||= User.find_by_steam_nickname session[:current_user][:nickname] if session[:current_user][:nickname]
+  end
 end
